9f788c008e6839cdba700332a540d95688f29729
alienao <armin.orujyan@gmail.com>
2021-07-13 00:25:50 +0400

    build: (weather-app)

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import sunny from "./images/01.svg";
+import halfSunny from "./images/05.svg";
+import rainy from "./images/09.svg";
+// import sunnyRainy from "./images/17.svg";
+
+const weatherInfo = [
+  {
+    weekDay: "Monday",
+    imgURL: sunny,
+    temp: "36C",
+  },
+  {
+    weekDay: "Tuesday",
+    imgURL: halfSunny,
+    temp: "32C",
+  },
+  {
+    weekDay: "Wednesday",
+    imgURL: rainy,
+    temp: "35C",
+  },
+  {
+    weekDay: "Thursday",
+    imgURL: rainy,
+    temp: "45C",
+  },
+  {
+    weekDay: "Friday",
+    imgURL: rainy,
+    temp: "36C",
+  },
+  {
+    weekDay: "Saturday",
+    imgURL: sunny,
+    temp: "37C",
+  },
+  {
+    weekDay: "Sunday",
+    imgURL: halfSunny,
+    temp: "39C",
+  },
+];
+const allDays = weatherInfo.map((el) => {
+  return (
+    <div className="weekDays">
+      <p className="WeekDay">{el.weekDay}</p>
+      <img className="image" src={el.imgURL} />
+      <p className="temp">{el.temp}</p>
+    </div>
+  );
+});
+
+function App() {
+  return <div className="App">{allDays}</div>;
+}
+
+export default App;
